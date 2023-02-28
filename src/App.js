9527c34819ae3ccd42ab013a4a51804125057f16
Gubeidulin Timur <timur3009@yandex.ru>
2023-02-28 21:49:42 +0300

    skeleton jsx ready + new pages started

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import "./App.css";
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from './components/PizzaBlock';
+import Home from './pages/Home';
+import NotFound from "./pages/NotFound";
 
-import pizzaCatalogue from './assets/pizzas.json';
+import React from "react";
 
 function App() {
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {pizzaCatalogue.map((pizzaItem) => (
-                        <PizzaBlock 
-                        key={pizzaItem.id}
-         {... pizzaItem}
-                        />
-                        ))}
-          </div>
+<NotFound />
         </div>
       </div>
     </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from './NotFoundBlock.module.scss';
+
+const NotFoundBlock = () => {
+    return (
+        <div>
+            <h1 className={styles.root}>
+                <span>Увы....</span>
+                <br />
+                Ничего не найдено
+            </h1>
+        </div>
+    )
+}
+
+export default NotFoundBlock;
